e7308996225a6083214d9d1fcfe38141857ac9e7
Bryan <bryan.burgers@gmail.com>
2011-07-15 13:21:39 -0500

    Added wind, wind direction, and gusts to the parser.

--- weatherParser.js
+++ weatherParser.js
@@ -16,16 +16,23 @@ SecondGriffin = window.SecondGriffin || { };
       var tempsTr = hoursTr.nextElementSibling;
       var dewpsTr = tempsTr.nextElementSibling;
       var heatsTr = dewpsTr.nextElementSibling;
-
+      var windsTr = heatsTr.nextElementSibling;
+      var winddTr = windsTr.nextElementSibling;
+      var gustsTr = winddTr.nextElementSibling;
 
       var t = function(el, i) { return el.childNodes[i].innerText; }
 
       for (var i = 1; i < datesTr.childNodes.length; i++) {
+        console.log(i);
         var date = t(datesTr, i) || lastDate;
         var hour = parseInt(t(hoursTr, i), 10);
         var temp = parseInt(t(tempsTr, i), 10);
         var dewp = parseInt(t(dewpsTr, i), 10);
         var heat = parseInt(t(heatsTr, i) || temp, 10);
+	var wind = parseInt(t(windsTr, i), 10);
+	var windDir = t(winddTr, i);
+	var gustT = t(gustsTr, i);
+	var gust = gustT ? parseInt(gustT, 10) : null;
 
         lastDate = date;
 
@@ -34,7 +41,10 @@ SecondGriffin = window.SecondGriffin || { };
           "hour": hour,
           "temp": temp,
           "apparent": heat,
-          "dew": dewp
+          "dew": dewp,
+	  "wind": wind,
+	  "windDirection": windDir,
+	  "gust": gust
         });
       }
     }
