6751293ab64502ccf1ba52946e29390abf076310
Bryan <bryan.burgers@gmail.com>
2011-07-15 15:36:12 -0500

    Graph overhaul and wind addition
    In order to switch to wind graph, I needed to overhaul how the
    WeatherGraph worked. This included adding a GraphSettings class
    that now handles translations, as well as all of the dimensional
    information about the graph.
    
    Also, I needed to add some buttons on the page to switch
    between the modes. These don't look great right now, but they
    will do for now.

--- graph.js
+++ graph.js
@@ -5,6 +5,13 @@ SecondGriffin = window.SecondGriffin || { };
   var dewColor = "rgb(0, 153, 0)";
   var heatColor = "rgb(184, 134, 11)";
   var tempColor = "rgb(255, 0, 0)";
+  var gustColor = "rgb(0, 0, 102)";
+  var windColor = "rgb(153, 0, 153)";
+  var windVeinColor = "rgb(102, 102, 102)";
+
+  var convertToKnots = function(i) {
+    return i * 1.25;
+  };
 
   var drawLine = function(ctx, x1, y1, x2, y2, color) {
     ctx.strokeStyle = color;
@@ -18,6 +25,40 @@ SecondGriffin = window.SecondGriffin || { };
     ctx.beginPath();
     ctx.arc(x, y, 2, 0, 2 * Math.PI, 0);
     ctx.fill();
+  };
+
+  var drawTextAbove = function(ctx, x, y, text) {
+    if (ctx.fillText && ctx.measureText) {
+      var measurement = ctx.measureText(text);
+      ctx.fillText(text, x - (measurement.width / 2), y - 5);
+    }
+  };
+
+  var drawTextBelow = function(ctx, x, y, text) {
+    if (ctx.fillText && ctx.measureText) {
+      var measurement = ctx.measureText(text);
+      ctx.fillText(text, x - (measurement.width / 2), y + 15);
+    }
+  };
+
+  var GraphSettings = function(xBounds, yBounds, rectangle, gridWidth, gridHeight) {
+    this.bounds = { "x": xBounds, "y": yBounds };
+    this.graphArea = rectangle;
+    this.grid = { "width": gridWidth, "height": gridHeight };
+  }
+  GraphSettings.prototype.translateX = function(i) {
+    var tx = (i + 1 - this.bounds.x.min) * this.grid.width + this.graphArea.x; // The actual value.
+    return Math.round(tx) - 0.5; // Rendering looks better if it's exactly on a half pixel.
+  };
+  GraphSettings.prototype.translateY = function(i) {
+    var ty = this.graphArea.bottom - ((i - this.bounds.y.min) * this.grid.height); // The actual value.
+    return Math.round(ty) - 0.5; // Rendering looks better if it's exactly on a half pixel.
+  };
+
+  var Bounds = function(min, max, by) {
+    this.min = min;
+    this.max = max;
+    this.by = by;
   };
 
   var Rectangle = function(x, y, width, height) {
@@ -60,6 +101,7 @@ SecondGriffin = window.SecondGriffin || { };
   };
 
   graph.TEMPERATURE = 1;
+  graph.WIND = 2;
 
   graph.prototype.getGraphCanvas = function() {
     return this.graphCanvas;
@@ -69,15 +111,6 @@ SecondGriffin = window.SecondGriffin || { };
   };
   graph.prototype.getKeyCanvas = function() {
     return this.keyCanvas;
-  };
-
-  graph.prototype.setLogicalYBounds = function(min, max) {
-    this.logicalMinY = min;
-    this.logicalMaxY = max;
-  };
-  graph.prototype.setLogicalXBounds = function(min, max) {
-    this.logicalMinX = min;
-    this.logicalMaxX = max;
   };
 
   // Set the width of a grid. This size will be used regardless, and the graph
@@ -115,9 +148,17 @@ SecondGriffin = window.SecondGriffin || { };
     var keyCanvas = this.keyCanvas;
     var kctx = keyCanvas.getContext("2d");
 
-    this.setLogicalXBounds(0, data.length - 1);
+    var bounds = { "y": new Bounds(0, 100, 10), "x": new Bounds(0, data.length + 1, 1) };
+    if (!mode || mode == graph.TEMPERATURE) {
+      bounds.y = this.getTemperatureLogicalYBounds(data);
+    }
+    else if (mode == graph.WIND) {
+      bounds.y = this.getWindLogicalYBounds(data);
+    }
 
     canvas.width = (data.length + 1) * this.gridWidth + this.leftSpacing + this.rightSpacing;
+    canvas.height = ((bounds.y.max - bounds.y.min) / bounds.y.by) * this.maximumGridHeight + this.topSpacing + this.bottomSpacing + this.horizontalAxisHeight;
+    yAxisCanvas.height = ((bounds.y.max - bounds.y.min) / bounds.y.by) * this.maximumGridHeight + this.topSpacing + this.bottomSpacing + this.horizontalAxisHeight;
 
     var rect = new Rectangle(
       this.leftSpacing,
@@ -125,27 +166,37 @@ SecondGriffin = window.SecondGriffin || { };
       canvas.width - this.leftSpacing - this.rightSpacing,
       canvas.height - this.topSpacing - this.bottomSpacing - this.horizontalAxisHeight);
 
-    this.yspacing = rect.height / (this.logicalMaxY - this.logicalMinY);
+    var keyRect = new Rectangle(0, 0, keyCanvas.width, keyCanvas.height);
 
-    this.drawBackground(ctx, data, rect);
-    this.drawLines(ctx, data, rect);
-    //this.drawData(ctx, data, rect);
-    this.drawTemperatureData(ctx, data, rect);
-    this.drawTemperatureKey(kctx, new Rectangle(0, 0, keyCanvas.width, keyCanvas.height));
-    this.drawXAxis(ctx, data, rect);
-    this.drawYAxis(yctx, data, rect);
+    var graphSettings = new GraphSettings(bounds.x, bounds.y, rect, rect.width / (bounds.x.max - bounds.x.min), rect.height / (bounds.y.max - bounds.y.min));
+
+    this.drawBackground(ctx, data, graphSettings);
+    this.drawLines(ctx, data, graphSettings);
+
+    if (!mode || mode == graph.TEMPERATURE) {
+      this.drawTemperatureData(ctx, data, graphSettings);
+      this.drawTemperatureKey(kctx, keyRect);
+    }
+    else if (mode == graph.WIND) {
+      this.drawWindData(ctx, data, graphSettings);
+      this.drawWindKey(kctx, keyRect);
+    }
+
+    this.drawXAxis(ctx, data, graphSettings);
+    this.drawYAxis(yctx, data, graphSettings);
   };
 
-  graph.prototype.drawBackground = function(ctx, data, rect) {
+  graph.prototype.drawBackground = function(ctx, data, graphSettings) {
     ctx.save();
+    var rect = graphSettings.graphArea;
 
     var gradient = ctx.createLinearGradient(0, 0, 0, rect.height);
     ctx.fillStyle = this.dayBackground;
     ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
 
     var fillBackground = function(i) {
-      var xStart = this.translateX(i - 0.5, rect);
-      var xEnd = this.translateX(i + 12.5, rect);
+      var xStart = graphSettings.translateX(i - 0.5, rect);
+      var xEnd = graphSettings.translateX(i + 12.5, rect);
     
       var gradient2 = ctx.createLinearGradient(xStart, 0, xEnd, 0);
       gradient2.addColorStop(0, this.dayBackground);
@@ -183,24 +234,26 @@ SecondGriffin = window.SecondGriffin || { };
     ctx.restore();
   };
 
-  graph.prototype.drawLines = function(ctx, data, rect) {
+  graph.prototype.drawLines = function(ctx, data, graphSettings) {
     ctx.save();
 
+    var rect = graphSettings.graphArea;
+
     ctx.strokeStyle = this.gridlineColor;
-    var startY = Math.floor(this.logicalMinY / 10) * 10;
-    for (var y = startY; y <= this.logicalMaxY; y += 10) {
-      var ty = this.translateY(y, rect);
+    var startY = Math.floor(graphSettings.bounds.y.min / 10) * 10;
+    for (var y = startY; y <= graphSettings.bounds.y.max; y += graphSettings.bounds.y.by) {
+      var ty = graphSettings.translateY(y);
       ctx.beginPath();
-      ctx.moveTo(rect.left, ty);
-      ctx.lineTo(rect.right, ty);
+      ctx.moveTo(graphSettings.graphArea.left, ty);
+      ctx.lineTo(graphSettings.graphArea.right, ty);
       ctx.stroke();
     }
 
     for (var x = 0; x < data.length; x++) {
-      var tx = this.translateX(x, rect);
+      var tx = graphSettings.translateX(x);
       ctx.beginPath();
-      ctx.moveTo(tx, rect.top);
-      ctx.lineTo(tx, rect.bottom);
+      ctx.moveTo(tx, graphSettings.graphArea.top);
+      ctx.lineTo(tx, graphSettings.graphArea.bottom);
       ctx.stroke();   
     }
 
@@ -217,7 +270,7 @@ SecondGriffin = window.SecondGriffin || { };
     ctx.restore();
   }
 
-  graph.prototype.drawXAxis = function(ctx, data, rect) {
+  graph.prototype.drawXAxis = function(ctx, data, graphSettings) {
     var drawCenteredText = function(ctx, tx, ty, text) {
       if (ctx.fillText && ctx.measureText) {
         var measurement = ctx.measureText(text);
@@ -242,8 +295,8 @@ SecondGriffin = window.SecondGriffin || { };
 
     for (var i = 0; data[i]; i++) {
       if (data[i].hour % 3 == 1) {
-        var tx = this.translateX(i, rect);
-        var ty = rect.bottom + 10;
+        var tx = graphSettings.translateX(i);
+        var ty = graphSettings.graphArea.bottom + 10;
         if (i == 0) {
           // If it's really the first one, then we can't center it, because
           // then our line for the date will cut right though it.
@@ -255,14 +308,14 @@ SecondGriffin = window.SecondGriffin || { };
         }
       }
       if (data[i].hour == 0 || i == 0) {
-        var tx = this.translateX(i, rect);
-        var ty = rect.bottom + 22;
+        var tx = graphSettings.translateX(i);
+        var ty = graphSettings.graphArea.bottom + 22;
         drawLeftAlignedText(ctx, tx + 3, ty, data[i].day);
 
         ctx.save();
         ctx.strokeStyle = this.outlineColor;
         ctx.beginPath();
-        ctx.moveTo(tx, rect.bottom);
+        ctx.moveTo(tx, graphSettings.graphArea.bottom);
         ctx.lineTo(tx, ty + 5);
         ctx.stroke();
         ctx.restore();
@@ -272,13 +325,13 @@ SecondGriffin = window.SecondGriffin || { };
     ctx.restore();
   };
 
-  graph.prototype.drawYAxis = function(ctx, data, rect) {
+  graph.prototype.drawYAxis = function(ctx, data, graphSettings) {
     var drawRightAlignedText = function(ctx, tx, ty, text) {
       if (ctx.fillText && ctx.measureText) {
         var measurement = ctx.measureText(text);
         ctx.fillText(text, tx - measurement.width, ty);
       }
-    };    
+    };
 
     ctx.save();
     ctx.fillStyle = "rgba(255,255,255,0.75)";
@@ -287,92 +340,104 @@ SecondGriffin = window.SecondGriffin || { };
 
     ctx.save();    
 
-    var startY = Math.floor(this.logicalMinY / 10) * 10;
-    for (var y = startY; y <= this.logicalMaxY; y += 10) {
-      var ty = this.translateY(y, rect);                 
+    var startY = Math.floor(graphSettings.bounds.y.min / 10) * 10;
+    for (var y = startY; y <= graphSettings.bounds.y.max; y += graphSettings.bounds.y.by) {
+      var ty = graphSettings.translateY(y);                 
       drawRightAlignedText(ctx, 25, ty + 3, y.toString());
     }
 
     ctx.restore();
   };
 
-  graph.prototype.drawTemperatureData = function(ctx, data, rect) {
+  graph.prototype.getTemperatureLogicalYBounds = function(data, rect) {
+    if (data.length > 0) {
+      var minY = 1000;
+      var maxY = -200;
+      for (var i = 0; data[i]; i++) {
+        var temp = data[i].temp;
+        var apparent = data[i].apparent;
+        var dew = data[i].dew;
+  
+        var max = Math.max(temp, apparent, dew);
+        var min = Math.min(temp, apparent, dew);
+        var logMax = Math.ceil((max + 0.5) / 10) * 10;
+        var logMin = Math.floor((min - 0.5) / 10) * 10;
+        minY = Math.min(minY, logMin);
+        maxY = Math.max(maxY, logMax);
+      }
+  
+      return {
+        "min": minY,
+	"max": maxY,
+	"by": 10
+      };
+    }
+  };
+
+  graph.prototype.drawTemperatureData = function(ctx, data, graphSettings) {
     ctx.save();
-
-    var drawTextAbove = function(x, y, text, color) {
-      if (ctx.fillText && ctx.measureText) {
-        ctx.fillStyle = color;
-        var measurement = ctx.measureText(text);
-        ctx.fillText(text, x - (measurement.width / 2), y - 5);
-      }
-    };
-
-    var drawTextBelow = function(x, y, text, color) {
-      if (ctx.fillText && ctx.measureText) {
-        ctx.fillStyle = color;
-        var measurement = ctx.measureText(text);
-        ctx.fillText(text, x - (measurement.width / 2), y + 15);
-      }
-    };
 
     // Draw lines.
     for (var i = 1; data[i]; i++) {
       var x1, x2, y1, y2;
 
       // Draw dew line
-      x1 = this.translateX(i - 1, rect);
-      x2 = this.translateX(i, rect);
-      y1 = this.translateY(data[i - 1].dew, rect);
-      y2 = this.translateY(data[i].dew, rect);
+      x1 = graphSettings.translateX(i - 1);
+      x2 = graphSettings.translateX(i);
+      y1 = graphSettings.translateY(data[i - 1].dew);
+      y2 = graphSettings.translateY(data[i].dew);
       drawLine(ctx, x1, y1, x2, y2, dewColor);
 
       // Draw heat index line, if necessary
       // Only draw the line if it's different from the temperature line.
       if (data[i].apparent != data[i].temp || data[i-1].apparent != data[i-1].temp) {
-        y1 = this.translateY(data[i - 1].apparent, rect);
-        y2 = this.translateY(data[i].apparent, rect);
+        y1 = graphSettings.translateY(data[i - 1].apparent);
+        y2 = graphSettings.translateY(data[i].apparent);
         drawLine(ctx, x1, y1, x2, y2, heatColor);
       }
 
       // Draw temp line
-      y1 = this.translateY(data[i - 1].temp, rect);
-      y2 = this.translateY(data[i].temp, rect);
+      y1 = graphSettings.translateY(data[i - 1].temp);
+      y2 = graphSettings.translateY(data[i].temp);
       drawLine(ctx, x1, y1, x2, y2, tempColor);
     }
 
     // Draw dots and text
     for (var i = 0; data[i]; i++) {
       ctx.fillStyle = "black";
-      var x = this.translateX(i, rect);
+      var x = graphSettings.translateX(i);
       var dewY, heatY, tempY;
       
       // Draw dew point, if necessary
       if (data[i].dew != data[i].temp) {
-        dewY = this.translateY(data[i].dew, rect);
+        dewY = graphSettings.translateY(data[i].dew);
         drawPoint(ctx, x, dewY);
       }
 
       // Draw heat index point, if necessary
       if (data[i].apparent != data[i].temp) {
-        heatY = this.translateY(data[i].apparent, rect);
+        heatY = graphSettings.translateY(data[i].apparent);
         drawPoint(ctx, x, heatY);
       }
 
       // Draw temperature point
-      tempY = this.translateY(data[i].temp, rect);
+      tempY = graphSettings.translateY(data[i].temp);
       drawPoint(ctx, x, tempY);
 
       if (data[i].hour % 3 == 1) {
         // Draw dew text, if necessary
         if (data[i].dew != data[i].temp) {
-	  drawTextBelow(x, dewY, data[i].dew, dewColor);
+	  ctx.fillStyle = dewColor;
+	  drawTextBelow(ctx, x, dewY, data[i].dew);
 	}
 
 	if (data[i].apparent != data[i].temp) {
-	  drawTextAbove(x, heatY, data[i].apparent, heatColor);
+	  ctx.fillStyle = heatColor;
+	  drawTextAbove(ctx, x, heatY, data[i].apparent);
 	}
 
-	drawTextAbove(x, tempY, data[i].temp, tempColor);
+        ctx.fillStyle = tempColor;
+	drawTextAbove(ctx, x, tempY, data[i].temp);
       }
     }
 
@@ -380,6 +445,206 @@ SecondGriffin = window.SecondGriffin || { };
   };
 
   graph.prototype.drawTemperatureKey = function(ctx, rect) {
+    var keyItems = [
+      { color: tempColor, name: "Temperature" },
+      { color: heatColor, name: "Heat index" },
+      { color: dewColor, name: "Dew point" }
+    ];
+    this.drawKey(ctx, rect, keyItems);
+  };
+
+  graph.prototype.getWindLogicalYBounds = function(data, rect) {
+    var maxY = 10;
+    for (var i = 0; data[i]; i++) {
+      var wind = data[i].wind;
+      var gust = data[i].gust || 0;
+  
+      var max = Math.max(wind, gust);
+      var logMax = Math.ceil((max + 0.5) / 10) * 10;
+      maxY = Math.max(maxY, logMax);
+    }
+  
+    return {
+      "min": 0,
+      "max": maxY,
+      "by": 10
+    };
+  };
+
+  graph.prototype.drawWindData = function(ctx, data, graphSettings) {
+    ctx.save();
+
+    var drawWindVein = function(ctx, x, y, wind, direction) {
+      var knots = wind;
+
+      ctx.save();
+      var rotation = 5;
+
+      switch(direction) {
+        case "N":
+	  rotation = 0;
+	  break;
+	case "NNE":
+	  rotation = 22.5;
+	  break;
+	case "NE":
+	  rotation = 45;
+	  break;
+	case "ENE":
+	  rotation = 67.5;
+	  break;
+	case "E":
+	  rotation = 90;
+	  break;
+	case "ESE":
+	  rotation = 112.5;
+	  break;
+	case "SE":
+	  rotation = 135;
+	  break;
+	case "SSE":
+	  rotation = 157.5;
+	  break;
+	case "S":
+	  rotation = 180;
+	  break;
+	case "SSW":
+	  rotation = 202.5;
+	  break;
+	case "SW":
+	  rotation = 225;
+	  break;
+	case "WSW":
+	  rotation = 247.5;
+	  break;
+	case "W":
+	  rotation = 270;
+	  break;
+	case "WNW":
+	  rotation = 292.5;
+	  break;
+	case "NW":
+	  rotation = 315;
+	  break;
+	case "NNW":
+	  rotation = 337.5;
+	  break;
+      }
+
+      ctx.translate(x, y);
+      ctx.rotate(Math.PI * rotation / 180);
+      ctx.translate(-x, -y);
+
+
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+      ctx.lineTo(x, y - 20);
+
+      if (knots > 0) {
+        if (knots <= 5) {
+	  ctx.moveTo(x, y - 20);
+	  ctx.lineTo(x - 4, y - 20);
+	}
+	else {
+	  ctx.moveTo(x, y - 20);
+	  ctx.lineTo(x - 8, y - 20);
+	}
+      }
+
+      if (knots > 10) {
+        if (knots <= 15) {
+	  ctx.moveTo(x, y - 18);
+	  ctx.lineTo(x - 4, y - 18);
+	}
+	else {
+	  ctx.moveTo(x, y - 18);
+	  ctx.lineTo(x - 8, y - 18);
+	}
+      }
+
+      if (knots > 20) {
+        if (knots <= 25) {
+	  ctx.moveTo(x, y - 16);
+	  ctx.lineTo(x - 4, y - 16);
+	}
+	else {
+	  ctx.moveTo(x, y - 16);
+	  ctx.lineTo(x - 8, y - 16);
+	}
+      }
+
+      ctx.stroke();
+
+      ctx.restore();
+    };
+
+    // Draw lines.
+    for (var i = 1; data[i]; i++) {
+      var x1, x2, y1, y2;
+
+      x1 = graphSettings.translateX(i - 1);
+      x2 = graphSettings.translateX(i);
+
+      // Draw gust line
+      if (!!data[i - 1].gust && !!data[i].gust) {
+        y1 = graphSettings.translateY(data[i - 1].gust);
+        y2 = graphSettings.translateY(data[i].gust);
+        drawLine(ctx, x1, y1, x2, y2, gustColor);
+      }
+
+      // Draw wind line
+      y1 = graphSettings.translateY(data[i - 1].wind);
+      y2 = graphSettings.translateY(data[i].wind);
+      drawLine(ctx, x1, y1, x2, y2, windColor);
+
+    }
+
+    // Draw dots, veins, and text
+    for (var i = 0; data[i]; i++) {
+      ctx.fillStyle = "black";
+      var x = graphSettings.translateX(i);
+      var gustY, windY;
+      
+      // Draw gust point, if necessary
+      if (!!data[i].gust && data[i].gust != data[i].wind) {
+        gustY = graphSettings.translateY(data[i].gust);
+        drawPoint(ctx, x, gustY);
+      }
+
+      // Draw wind vein
+      windY = graphSettings.translateY(data[i].wind);
+      ctx.strokeStyle = windVeinColor;
+      drawWindVein(ctx, x, windY, data[i].wind, data[i].windDirection);
+
+      // Draw wind point
+      drawPoint(ctx, x, windY);
+
+
+      // Draw text
+      if (data[i].hour % 3 == 1) {
+        // Draw gust text, if necessary
+        if (!!data[i].gust && data[i].gust != data[i].wind) {
+	  ctx.fillStyle = gustColor;
+	  drawTextBelow(ctx, x, gustY, data[i].gust);
+	}
+
+        ctx.fillStyle = windColor;
+	drawTextBelow(ctx, x, windY, data[i].wind);
+      }
+    }
+
+    ctx.restore();
+  };
+
+  graph.prototype.drawWindKey = function(ctx, rect) {
+    var keyItems = [
+      { color: windColor, name: "Wind" },
+      { color: gustColor, name: "Gusts" },
+    ];
+    this.drawKey(ctx, rect, keyItems);
+  };
+
+  graph.prototype.drawKey = function(ctx, rect, keyItems) {
     ctx.save();
     ctx.clearRect(rect.x, rect.y, rect.width, rect.height);
 
@@ -389,11 +654,6 @@ SecondGriffin = window.SecondGriffin || { };
 
     var initialOffset = 5.5;
     var offset = 15;
-    var keyItems = [
-      { color: tempColor, name: "Temperature" },
-      { color: heatColor, name: "Heat index" },
-      { color: dewColor, name: "Dew point" }
-    ];
 
     var drawText = function(x, y, text, color) {
       if (ctx.fillText && ctx.measureText) {
