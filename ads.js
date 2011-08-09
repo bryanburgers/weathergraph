4b62bd43cb9edecaad880fc6da25fedc8313cf26
Bryan <bryan.burgers@gmail.com>
2011-08-09 12:22:46 -0500

    Rudimentary ad switching

--- ads.js
+++ ads.js
@@ -7,20 +7,74 @@ SecondGriffin = window.SecondGriffin || { };
 
   var spacerStyle = 'width: 100%; padding-top: 0px; padding-right: 0px; padding-bottom: 0px; padding-left: 0px; margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; border-top-width: 0px; border-right-width: 0px; border-bottom-width: 0px; border-left-width: 0px; border-style: initial; border-color: initial; position: static; background-image: none; background-attachment: initial; background-origin: initial; background-clip: initial; background-color: initial; height: 48px; background-position: initial initial; background-repeat: initial initial; ';
 
+  var linkStyle = 'width: 100%; height: 100%; padding: 0; margin: 0; display: block; color: white; text-decoration: none;';
+
   var containerId = 'ad';
 
+  var customAds =
+    [ { weight: 4.0, messages: ["Tired of ads?"] }
+    , { weight: 0.2, messages: ["Be kind, rewind", "Or something like that"] }
+    , { weight: 1.0, messages: ["Cheaper than a Big Mac"] }
+    , { weight: 1.0, messages: ["Cheaper than gallon of gas"] }
+    , { weight: 0.2, messages: ["Faster than a speeding bullet", "Oh wait, that's Superman"] }
+    ]
+
+  function chooseCustomAd() {
+    var sum = 0;
+    for (var i = 0; i < customAds.length; i++) {
+      sum += customAds[i].weight;
+    }
+    var r = Math.random() * sum;
+    for (var i = 0; i < customAds.length; i++) {
+      var ad = customAds[i];
+      if (r < ad.weight) {
+        return ad;
+      }
+      else {
+        r -= ad.weight;
+      }
+    }
+    return null;
+  }
+
   document.addEventListener('DOMContentLoaded', function() {
-    console.log('Loaded');
     var container = document.getElementById(containerId);
-    console.log('Creating');
+
     var div1 = document.createElement('div');
     div1.style.cssText = originalStyle;
+
     var div2 = document.createElement('div');
     div2.style.cssText = spacerStyle;
 
-    div1.style.backgroundColor = '#248';
+    var a = document.createElement('a');
+    a.style.cssText = linkStyle;
+    a.href = "https://market.android.com/details?id=com.secondgriffin.WeatherGraph";
+
+    div1.style.backgroundColor = '#48f';
+    div1.style.color = "#fff";
+    div1.appendChild(a);
+    a.innerHTML = "Upgrade to Weather Graph Pro!";
+
     container.appendChild(div1);
     container.appendChild(div2);
+
+    // Ad flipping
+    var ad = chooseCustomAd();
+    var messages = ad.messages.slice(0);
+    messages.push('Upgrade to Weather Graph Pro!');
+    var messageIndex = messages.length - 1;
+    var interval = 0;
+    var switchAd = function() {
+      console.log('Switching');
+      messageIndex = (messageIndex + 1) % messages.length;
+      a.innerHTML = messages[messageIndex];
+    };
+    var dispose = function() {
+      clearInterval(interval);
+    };
+    div1.dispose = dispose;
+    var interval = setInterval(switchAd, 4000);
+    switchAd();
   }, false);
 
   function fetch() {
@@ -36,6 +90,9 @@ SecondGriffin = window.SecondGriffin || { };
       if (ad.childNodes.length > 0) {
         // We have an ad, so replace the old ad with the new ad.
         while (container.firstChild != ad) {
+	  if (container.firstChild.dispose) {
+	    container.firstChild.dispose();
+	  }
           container.removeChild(container.firstChild);
         }
         ad.style.display = "block";
@@ -56,5 +113,6 @@ SecondGriffin = window.SecondGriffin || { };
 
   ns.AdManager = {
     'fetch': fetch
+    ,'chooseCustomAd': chooseCustomAd
   };
 })(SecondGriffin);
